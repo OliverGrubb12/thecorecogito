53f7615d8ddcdd559362cfb4ba2ec2329d17b8aa
OliverGrubb12 <oligru03@kingshigh.school.nz>
2025-09-17 14:01:59 +1200

    Meltdown Animation and Crystals
    read

--- addons/cogito/PackedScenes/progress_bar.gd
+++ addons/cogito/PackedScenes/progress_bar.gd
@@ -3,6 +3,7 @@ extends ProgressBar
 
 @onready var decrease_timer: Timer = $Timer
 @export var decrease_speed: float = 0.1 # Units per second
+@export var meltdown_video : VideoStreamPlayer
 
 
 func _ready():
@@ -20,6 +21,9 @@ func decrease_time() -> void:
 		#crystal.deposit_crystal.connect(_on_deposit_crystal)
 		
 	if value == 0:
+		decrease_timer.stop()
+		meltdown_video.play()
+		await meltdown_video.finished
 		get_tree().change_scene_to_file("res://addons/cogito/DemoScenes/COGITO_0_MainMenu.tscn")
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
